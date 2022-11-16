4b985250e9d0a142d3d5e71c05add0e7505498a6
Alejandro <75887246+ARMoreno99@users.noreply.github.com>
2022-11-16 17:34:13 +0100

    Add files via upload

---
+++ BANDIT/decompress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+xxd -r data.txt > data
+
+name_compressed=$(7z l data | grep "Name" -A 2 | tail -n 1 | awk 'NF{print $NF}')
+7z x data > /dev/null 2>&1
+
+while true; do
+	7z l $name_compressed > /dev/null 2>&1
+	
+	if [ "$(echo $?)" == "0" ];then
+		decompressed_next=$(7z l $name_compressed | grep "Name" -A 2 | tail -n 1 | awk 'NF{print $NF}')
+		7z x $name_compressed > /dev/null 2>&1 && name_compressed=$decompressed_next
+	else
+		cat $name_compressed | awk '{print $4}'; rm data* 2>/dev/null
+		exit 1
+	fi
+done
